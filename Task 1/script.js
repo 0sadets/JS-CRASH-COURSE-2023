83681d61d4d67e92dc466ee539078e85b35d34a9
0sadets <102857086+0sadets@users.noreply.github.com>
2023-03-21 20:28:08 +0200

    First task update

--- Task 1/script.js
+++ Task 1/script.js
@@ -30,3 +30,22 @@ if(sumDollar<0){
   alert("Invalid data");
 }
 alert( sumDollar + " = " + (sumDollar*0.95) );
+
+//fifth
+let GB = prompt("Enter the capacity of a flash drive(gb):");
+let MB = GB*1024;
+alert("You can save there " + Math.floor(MB/820) + " files");
+
+//additional practice
+// 1 task
+
+const a = prompt("Enter distance in miles: ");
+alert(a + " miles is " + a/0.621371);
+
+//2 task
+let hours = prompt("Enter current hour: ");
+let minutes = prompt("Enter current minute: ");
+let hoursLeft = 23 - hours;
+let minutesLeft = 60 - minutes;
+alert(hoursLeft + " hours and " + minutesLeft +" minutes left until the end of the day.");
+
